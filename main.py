a7437b0d53141680520424d5ee7f63185eeaa777
Anelius <47761414+Anelius@users.noreply.github.com>
2021-12-08 03:29:18 -0700

    Debugged match.py and added the cancel command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import match
 import nextcord
 from dotenv.main import load_dotenv
 from nextcord.ext import commands
@@ -6,15 +7,18 @@ from multiprocessing.connection import Listener
 from nextcord.ext.commands import view
 
 def main():
+# Variable declarations
+    # Bot variables
     load_dotenv()
     token = os.getenv("TOKEN")
     client = commands.Bot(command_prefix="!")
     bot_mod_role = 'Codi Boi'
 
+    # Logic Variables
     matches = {}
     match_players = {}
     queues = {}
-    queue_max = 8
+    queue_max = 2
     current_id = 0
 
     @client.event
@@ -35,7 +39,7 @@ def main():
         #         await message.channel.send('https://www.youtube.com/watch?v=N06fZxoUQx0')
         await client.process_commands(message)
 
-# User Commands -----------------------------------------------
+# Queue Commands -----------------------------------------------
     # Queue join command
     @client.command(aliases=['Q', 'queue', 'Queue'])
     async def q(ctx):
@@ -57,13 +61,13 @@ def main():
                 nonlocal current_id
                 players = queues.pop(ctx.channel)
 
-                match_players.update({current_id: players})
-                new_match = match(current_id, players)
+                match_players.update({f'{current_id}': players})
+                new_match = match.Match(f'{current_id}', players)
                 matches.update({f'{current_id}': new_match})
 
                 atMembers = f''
-                for user in matches[f'{current_id}']:
-                    atMembers += f'{user.mention} '
+                for player in players:
+                    atMembers += f'{player.mention} '
                 atMembers += f'\nYour match is now ready!\nYour match id is: {current_id}.'
                 await ctx.send(atMembers)
                 
@@ -100,20 +104,23 @@ def main():
         else:
             await ctx.send('This channel does not currently have an active queue.')
 
+# Selection mode commands
+    # Randomly selects teams
     @client.command(aliases=['R', 'random', 'Random'])
     async def r(ctx):
         found_id = is_in_match(ctx.author, match_players)
 
-        if fount_id == '?'
+        if found_id == '?':
             await ctx.send(f'{ctx.author.name} has no matches to vote in.')
         else:
             match_in = matches[found_id]
             if match_in.add_vote(ctx.author, 'random'):
-                await ctx.send(f'{ctx.author.name} has voted for random.')
+                await ctx.send(f"{ctx.author.name}'s vote has been updated.")
             else:
-                await ctx.send(f'{ctx.author.name} has already voted for random.')
+                await ctx.send(f'{ctx.author.name} has voted for: random team selection.')
 
-            match_in.count_votes(queue_max)
+            if match_in.count_votes(queue_max):
+                await ctx.send(f'Teams have been assigned for match of match id: {found_id}')
 
     @client.command(aliases=['C', 'captains', 'Captains'])
     async def c(ctx):
@@ -124,8 +131,26 @@ def main():
         await ctx.send('place holder text')
 
     @client.command()
+    async def o(ctx):
+        found_id = is_in_match(ctx.author, match_players)
+
+        if found_id == '?':
+            await ctx.send(f'{ctx.author.name} has no matches to vote in.')
+        else:
+            match_in = matches[found_id]
+            if match_in.add_vote(ctx.author, 'ordered'):
+                await ctx.send(f"{ctx.author.name}'s vote has been updated.")
+            else:
+                await ctx.send(f'{ctx.author.name} has voted for: order joined team selection.')
+
+            if match_in.count_votes(queue_max):
+                await ctx.send(f'Teams have been assigned for match of match id: {found_id}')
+
+# 
+    @client.command()
     async def active(ctx):
-        await ctx.send('place holder text')
+
+        await ctx.send('There are no unreported matches.')
     
     @client.command()
     async def report(ctx, id, result):
@@ -146,10 +171,13 @@ def main():
         else:
             await ctx.send(f'This channel does not currently have an active queue.')
 
+    # Clear active match based on the given id
     @client.command()
     @commands.has_role(bot_mod_role)
     async def cancel(ctx, id):
-        await ctx.send('place holder text')
+        match_players.pop(f'{id}')
+        matches.pop(f'{id}')
+        await ctx.send(f'Match of match id: {id} has been cancelled.')
 
     @client.command()
     @commands.has_role(bot_mod_role)
@@ -164,10 +192,10 @@ def main():
     client.run(token)
 # _______________________________________________ End of main _______________________________________________
 
-def is_in_match(player, players):
-    for match in players:
-        if player in match
-            return key=match.get
+def is_in_match(player, match_players):
+    for match in match_players:
+        if player in match_players[match]:
+            return match
     return '?'
 
 # Calls main (run with: "python3 main.py") __________________________________________________________________
--- match.py
+++ match.py
@@ -4,7 +4,8 @@ class Match:
     def __init__(self, id, players):
         self.id = id
         self.sorted = False
-        self.players = players
+        self.players = players # List
+        self.number_of_players = len(self.players)
         self.team_1 = []
         self.team_2 = []
         self.votes = {}
@@ -14,32 +15,33 @@ class Match:
         already_voted = False
 
         if player in self.votes:
-            self.votes.update({player:vote})
-            if self.vote_records[i] == player:
-                already_voted = True
-                voter_index = i
-        
-        if already_voted:
-            self.votes[voter_index] = vote
-        else:
-            self.votes.append(vote)
-            self.vote_records.append(player)
+            already_voted = True   
 
+        self.votes.update({player:vote})
         return already_voted
 
-    def vote_check(self):
+    def recorded_voters(self):
         voter_list = []
 
-        for player in self.players
-            if player in self.votes
+        for player in self.players:
+            if player in self.votes:
                 voter_list.append(player)
 
         return voter_list
 
+    def missing_voters(self):
+        missing_list = []
+
+        for player in self.players:
+            if not player in self.votes:
+                missing_list.append(player)
+
+        return missing_list
+
     def count_votes(self, queue_max):
         # Checks for less than number of member votes
-        if len(self.votes) < queue_max:
-            return False
+        #if len(self.votes) < queue_max:
+        #    return False
         
         vote_tally = {
             'r_count': 0,
@@ -50,13 +52,26 @@ class Match:
 
         for vote in self.votes.values():
             if vote == 'random':
-                vote_tally.update('r_count' : vote_tally.r_count+1)
+                vote_tally.update({'r_count': vote_tally['r_count']+1})
             elif vote == 'captains':
-                vote_tally.update('c_count' : vote_tally.r_count+1)
+                vote_tally.update({'c_count': vote_tally['c_count']+1})
             elif vote == 'balance':
-                vote_tally.update('b_count' : vote_tally.r_count+1)
+                vote_tally.update({'b_count': vote_tally['b_count']+1})
             elif vote == 'ordered':
-                vote_tally.update('o_count' : vote_tally.r_count+1)
+                vote_tally.update({'o_count': vote_tally['o_count']+1})
+
+        for tally in vote_tally:
+            if vote_tally[tally] >= self.number_of_players/2:
+                if vote_tally == 'r_count':
+                    self.randomize()
+                elif vote_tally == 'c_count':
+                    self.captains()
+                elif vote_tally == 'b_count':
+                    self.balance()
+                elif vote_tally == 'o_count':
+                    self.ordered()
+                
+                return True
 
         # Find the largest value (always returns the first highest instance of the max number)
         vote_win = max(vote_tally, key=vote_tally.get)
@@ -69,30 +84,30 @@ class Match:
                 max_counter += 1
 
         # Will result in "True" if a tie was found
-        if max_counter > 1
+        if max_counter > 1:
             # There was a tie found, must prompt the user for additional attention
             # NOTE: Ask what should happen here if no majority
             return False
 
         # Call method for winning selection mode
         if vote_win == 'r_count':
-            randomize()
+            self.randomize()
         elif vote_win == 'c_count':
-            captains()
+            self.captains()
         elif vote_win == 'b_count':
-            balance()
+            self.balance()
         elif vote_win == 'o_count':
-            ordered()
+            self.ordered()
         
         return True
     
 # Selection mode commands -------------------------------------------------
     def randomize(self):
-        unsorded_players = self.players
+        unsorted_players = self.players
         
-        for i in (0, len(self.players)/2):
-            self.team_1.append(unsorted_players.remove(unsorted_players[random.randint(0, len(self.players)-1)]))
-            self.team_2.append(unsorted_players.remove(unsorted_players[random.randint(0, len(self.players)-1)]))
+        for i in range(int(self.number_of_players/2)):
+            self.team_1.append(unsorted_players.pop(random.randint(0, len(unsorted_players)-1)))
+            self.team_2.append(unsorted_players.pop(random.randint(0, len(unsorted_players)-1)))
 
         self.sorted = True
 
@@ -110,10 +125,10 @@ class Match:
 
     # Assign teams based on the order they joined the queue
     def ordered(self):
-        for i in range(0, (len(self.players)/2)):
+        for i in range(0, (self.number_of_players/2)):
             self.team_1.append(players[i])
 
-        for i in range(len(self.players)/2, len(self.players)):
+        for i in range(self.number_of_players/2, self.number_of_players):
             self.team_2.append(players[i])
 
         self.sorted = True
